86a0d9826d76b3e6567ca605dd29d854e5dd2e2f
doonska <727187@student.dusd.net>
2025-03-06 11:42:35 -0800

    UI player control
    did scripts for the camera and movement

--- U1PlayerControlP4Villalobos/Assets/Scripts/PlayerController.cs
+++ U1PlayerControlP4Villalobos/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+    
 {
     public float speed = 20;
+    public float turnSpeed;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
 
     }
 }
